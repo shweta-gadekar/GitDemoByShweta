6181a6255c5c3ad8ae6857e56bf6d1db10017eda
GreeshmaMore@23 <shwetasgadekar@gmail.com>
2025-05-15 08:58:21 +0530

    added code

---
+++ src/StudentRegistration/RegistrationOStd.java
@@ -0,0 +1,10 @@
+package StudentRegistration;
+
+public class RegistrationOStd {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world....");
+		System.out.println("This is demo project .");
+	}
+
+}
